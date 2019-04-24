efaca0961a855235ef93080ef1ac6480eb7b114b
Miguel Alberto Plazas Wadynski <ambigus9@gmail.com>
2019-04-24 12:10:52 -0500

    preprocesar muestras para predecir

--- baseline.py
+++ baseline.py
@@ -13,16 +13,13 @@ import os
 base_model = ResNet50(weights='imagenet')
 model = Model(inputs=base_model.input, outputs=base_model.get_layer('fc1000').output)
 
-
 def normalizar(datos):
   scaler = StandardScaler()
   return scaler.fit_transform(datos)
 
 def training(X_train,y_train):
-  #Extraer características
   caracteristicas_train = deep_extractor(X_train)
   X_train = normalizar( np.array(caracteristicas_train) )
-
   # Entrenar SVM
   clf = SVC(probability=True, kernel='linear', C=1.0)
   clf.fit(X_train, y_train)
@@ -40,23 +37,15 @@ def deep_extractor(imagenes):
   return caracteristicas
 
 def labeling(clasificador,batch_set,EL,LC):
-  caracteristicas_batch_set = deep_extractor(batch_set)
+  caracteristicas_batch_set = normalizar( np.array( deep_extractor(batch_set) ) )
   y_pred = clasificador.predict(caracteristicas_batch_set)
   y_pred_proba = clasificador.predict_proba(caracteristicas_batch_set)
   return y_pred, y_pred_proba
 
 def test(X_test,y_test):
-  # Extraer Deep Features X_test
-  caracteristicas_test = []
-  for i in tqdm(X_test):
-    img = image.load_img( i , target_size=(224, 224))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
-    fc1000 = model.predict(x)
-    caracteristicas_test.append(fc1000[0])
-  
+  caracteristicas_test = deep_extractor(X_test)
   X_test = normalizar( np.array(caracteristicas_test) )
+  print("Score: ",clasificador.score(X_test,y_test))
   return X_test, y_test
 
 def split_dataset(L,metodo):
@@ -87,17 +76,22 @@ def split_dataset(L,metodo):
     print("  val: ",len(X_val))
     print(" test: ",len(X_test))
     print("total: ",len(X_train)+len(X_val)+len(X_test))
-    #return X_train, X_test, y_train, y_test, U_img
+
     return X_train, X_val, X_test, y_train, y_val, y_test
     
   if metodo=='semi-supervisado':
     L_img, U_img, L_class, U_class = train_test_split(imagenes, clases, test_size=0.5, random_state=1)
     X_train, X_test, y_train, y_test = train_test_split(L_img, L_class, test_size=0.4, random_state=1)
     X_train, X_val, y_train , y_val = train_test_split(X_train, y_train, test_size=0.66, random_state=1)
-  
+
+    U_img_ = U_img[:int(len(U_img)*porcentaje)]
+    U_class_ = U_class[:int(len(U_class)*porcentaje)]
+
+    print('\n')
     print("train: ",len(X_train))
     print("  val: ",len(X_val))
     print(" test: ",len(X_test))
+    print("U_img: ",len(U_img_))
     print("total etiquetado: ",len(X_train)+len(X_val)+len(X_test))
     print("total no-etiquetado: ",len(U_img))
     print("total global: ",len(X_train)+len(X_val)+len(X_test)+len(U_img))
@@ -114,14 +108,13 @@ count = 0
 
 L = 'ucmerced'
 batch_size = 0.2
+porcentaje=0.1
 
-X_train, X_val, X_test, y_train, y_val, y_test = split_dataset(L,'supervisado')
-print('\n')
-X_train_, X_val_, X_test_, y_train_, y_val_, y_test_, U_img_, U_class_ = split_dataset(L,'semi-supervisado')
+X_train, X_val, X_test, y_train, y_val, y_test, U_img, U_class = split_dataset(L,'semi-supervisado')
 
 clasificador = training(X_train,y_train)
 X_test_temp,y_test_temp = test(X_test,y_test)
-clasificador.score(X_test_temp,y_test_temp)
+
 
 batch_set = return_batch_set(U_img,batch_size)[count]
 
