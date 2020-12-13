c4108ebd17034e4e3f682478d23977df68d36485
ambigus9 <ambigus9@gmail.com>
2020-12-13 20:53:06 +0000

    fixing aug transform for satellital samples

--- ssl_satellital/ssl_train.py
+++ ssl_satellital/ssl_train.py
@@ -73,14 +73,14 @@ def training(kfold, etapa, datos, architecture, iteracion, models_info, pipeline
                       class_mode="categorical",
                       target_size=(pipeline['img_height'],pipeline['img_width']))
 
-    #if etapa == 'train' or etapa == 'train_EL':
+    
+    num_classes = len( datos["df_train"]["y_col_name"].unique() )
+
     if etapa == 'train' and pipeline["transfer_learning"] == "classic":
-        finetune_model = transfer_learning_classic(base_model,
-                                                len( datos["df_train"]["y_col_name"].unique() ))
+        finetune_model = transfer_learning_classic( base_model, num_classes )
     elif pipeline["transfer_learning"] == "soft":
-        finetune_model = transfer_learning_soft(base_model,
-                                                len( datos["df_train"]["y_col_name"].unique() ),
-                                                pipeline["stage_config"][iteracion])
+        finetune_model = transfer_learning_soft( base_model, num_classes,
+                                                 pipeline["stage_config"][iteracion] )
     else:
         finetune_model = base_model
     
